39d67e62f8aa2a4af8ff40c0482cc1501fa292c8
coma64 <zaton.tristan@gmail.com>
2021-03-05 12:37:15 +0100

    Fix build docs scripts

--- scripts/BuildDocs.ps1
+++ scripts/BuildDocs.ps1
@@ -2,7 +2,11 @@ param([switch]$ForceRebuild = $false)
 
 if ($ForceRebuild) {
     pdoc --html --output-dir docs nextcloud_notes_api --force
+    Move-Item -Force .\docs\nextcloud_notes_api\* .\docs
+    Remove-Item -Recurse -Force .\docs\nextcloud_notes_api
 }
 else {
     pdoc --html --output-dir docs nextcloud_notes_api
+    Move-Item -Force .\docs\nextcloud_notes_api\* .\docs
+    Remove-Item -Recurse -Force .\docs\nextcloud_notes_api
 }
--- scripts/build-docs.bash
+++ scripts/build-docs.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 pdoc --html --output-dir docs nextcloud_notes_api "$@"
+mv -f ./docs/nextcloud_notes_api/* ./docs
+rm -rf ./docs/nextcloud_notes_api
